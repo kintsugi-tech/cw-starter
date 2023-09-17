b1403553644c52b660c384f7e93ad188f0b9428d
dimiandre <dimiandre@gmail.com>
2023-09-17 11:41:57 +0200

    Revert "Update versions to build with latest cargo"
    This reverts commit c47a4a4c677805bbb1ed69098f705fb542ab6e24.

--- contracts/first-contract/src/msg.rs
+++ contracts/first-contract/src/msg.rs
@@ -1,18 +1,35 @@
+/*
+The **msg**.rs file typically contains the definition of the contract's message struct used in execute
+and query requests.
+*/
+
 use common::token::Token;
 use cosmwasm_schema::{cw_serde, QueryResponses};
+use cw_ownable::{cw_ownable_execute, cw_ownable_query};
 
 /// This struct contains required variables to instantiate a new contract.
 #[cw_serde]
-pub struct InstantiateMsg { }
+pub struct InstantiateMsg {
+    /// Contract owner address
+    pub owner: String,
+    /// Represents the tokens users are allowed to deposit.
+    pub allowed_token: Token,
+}
 
 /// This enum describes available contract's execution messages.
+#[cw_ownable_execute]
 #[cw_serde]
 pub enum ExecuteMsg {
     /// Allows to update the contract's `allowed_token`. Only owner can update.
-    BurnToken { },
+    UpdateToken { new_token: Token },
 }
 
 /// This enum describes available contract's query messages.
+#[cw_ownable_query]
 #[cw_serde]
 #[derive(QueryResponses)]
-pub enum QueryMsg { }
+pub enum QueryMsg {
+    /// Retrieve the contract allowed token.
+    #[returns[Token]]
+    AllowedToken {},
+}
